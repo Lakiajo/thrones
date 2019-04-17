eb8d7dbc793106ed5c1d18a281165c4ea493d485
Lakia Jones <rachandra_jones@yahoo.com>
2019-04-16 20:20:42 -0500

    WIP

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,10 @@
+import characters from './components/characters/characters';
 import '../styles/main.scss';
+
+const init = () => {
+  characters.getData();
+};
+
+init();
 
 console.error('Lakia Jones');
